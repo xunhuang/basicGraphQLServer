82ad2f6bdf481eda9d3d760621df70f9126a8942
Wilson Huang (Growth) <xhuang@fb.com>
2021-04-20 14:43:56 -0700

    save changes

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,103 @@
+import * as admin from 'firebase-admin';
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer, ApolloError, ValidationError, gql } from 'apollo-server-express';
+// import { ApolloServer, ApolloError, ValidationError, gql } from 'apollo-server';
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: "Parable of the Sower",
-    author: "Octavia E. Butler"
-  },
-  {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
-  }
-];
+
+const serviceAccount = require('../service-account.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+interface User {
+  id: string;
+  name: string;
+  screenName: string;
+  statusesCount: number;
+}
+
+interface Tweet {
+  id: string;
+  likes: number;
+  text: string;
+  userId: string;
+}
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+ # A Twitter User
+  type User {
+    id: ID!
+    name: String!
+    screenName: String!
+    statusesCount: Int!
+    tweets: [Tweets]!
   }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
+  # A Tweet Object
+  type Tweets {
+    id: ID!
+    text: String!
+    userId: String!
+    user: User!
+    likes: Int!
+  }
   type Query {
-    books: [Book]
+    tweets: [Tweets]
+    user(id: String!): User
   }
-`;
+  `;
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    async tweets() {
+      const tweets = await admin
+        .firestore()
+        .collection('tweets')
+        .get();
+      return tweets.docs.map(tweet => tweet.data()) as Tweet[];
+    },
+    async user(_: null, args: { id: string }) {
+      try {
+        const userDoc = await admin
+          .firestore()
+          .doc(`users/${args.id}`)
+          .get();
+        const user = userDoc.data() as User | undefined;
+        return user || new ValidationError('User ID not found');
+      } catch (error) {
+        throw new ApolloError(error);
+      }
+    }
+  },
+  User: {
+    async tweets(user: User) {
+      try {
+        const userTweets = await admin
+          .firestore()
+          .collection('tweets')
+          .where('userId', '==', user.id)
+          .get();
+        return userTweets.docs.map(tweet => tweet.data()) as Tweet[];
+      } catch (error) {
+        throw new ApolloError(error);
+      }
+    }
+  },
+  Tweets: {
+    async user(tweet: Tweet) {
+      try {
+        const tweetAuthor = await admin
+          .firestore()
+          .doc(`users/${tweet.userId}`)
+          .get();
+        return tweetAuthor.data() as User;
+      } catch (error) {
+        throw new ApolloError(error);
+      }
+    }
   }
 };
 
